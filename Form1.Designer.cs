f42f24d5885f90603602ff166d71bbbae7cda410
HaloImageEngine <william.d.beaty@gmail.com>
2022-10-08 12:58:19 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ImageSlider
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.txt_Count = new System.Windows.Forms.TextBox();
+            this.txt_Counter = new System.Windows.Forms.TextBox();
+            this.txt_FileName = new System.Windows.Forms.TextBox();
+            this.txt_Width = new System.Windows.Forms.TextBox();
+            this.txt_Height = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Folder = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_Timer = new System.Windows.Forms.TextBox();
+            this.txt_Type = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseMnemonic = false;
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_Stop.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Stop.Location = new System.Drawing.Point(12, 41);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // txt_Count
+            // 
+            this.txt_Count.Location = new System.Drawing.Point(12, 70);
+            this.txt_Count.Name = "txt_Count";
+            this.txt_Count.Size = new System.Drawing.Size(34, 23);
+            this.txt_Count.TabIndex = 2;
+            // 
+            // txt_Counter
+            // 
+            this.txt_Counter.Location = new System.Drawing.Point(52, 70);
+            this.txt_Counter.Name = "txt_Counter";
+            this.txt_Counter.Size = new System.Drawing.Size(35, 23);
+            this.txt_Counter.TabIndex = 3;
+            // 
+            // txt_FileName
+            // 
+            this.txt_FileName.Location = new System.Drawing.Point(105, 12);
+            this.txt_FileName.Name = "txt_FileName";
+            this.txt_FileName.Size = new System.Drawing.Size(189, 23);
+            this.txt_FileName.TabIndex = 4;
+            // 
+            // txt_Width
+            // 
+            this.txt_Width.Location = new System.Drawing.Point(41, 99);
+            this.txt_Width.Name = "txt_Width";
+            this.txt_Width.Size = new System.Drawing.Size(46, 23);
+            this.txt_Width.TabIndex = 5;
+            // 
+            // txt_Height
+            // 
+            this.txt_Height.Location = new System.Drawing.Point(41, 127);
+            this.txt_Height.Name = "txt_Height";
+            this.txt_Height.Size = new System.Drawing.Size(46, 23);
+            this.txt_Height.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(11, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "W";
+            // 
+            // txt_Folder
+            // 
+            this.txt_Folder.Location = new System.Drawing.Point(11, 156);
+            this.txt_Folder.Name = "txt_Folder";
+            this.txt_Folder.Size = new System.Drawing.Size(76, 23);
+            this.txt_Folder.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(105, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(670, 395);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_Timer
+            // 
+            this.txt_Timer.Location = new System.Drawing.Point(11, 185);
+            this.txt_Timer.Name = "txt_Timer";
+            this.txt_Timer.Size = new System.Drawing.Size(76, 23);
+            this.txt_Timer.TabIndex = 11;
+            // 
+            // txt_Type
+            // 
+            this.txt_Type.Location = new System.Drawing.Point(12, 214);
+            this.txt_Type.Name = "txt_Type";
+            this.txt_Type.Size = new System.Drawing.Size(76, 23);
+            this.txt_Type.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Type);
+            this.Controls.Add(this.txt_Timer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_Width);
+            this.Controls.Add(this.txt_Folder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Height);
+            this.Controls.Add(this.txt_FileName);
+            this.Controls.Add(this.txt_Counter);
+            this.Controls.Add(this.txt_Count);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "ImageSlider";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_Start;
+        private Button btn_Stop;
+        private TextBox txt_Count;
+        private TextBox txt_Counter;
+        private TextBox txt_FileName;
+        private TextBox txt_Width;
+        private TextBox txt_Height;
+        private Label label1;
+        private Label label2;
+        private TextBox txt_Folder;
+        private PictureBox pictureBox1;
+        private TextBox txt_Timer;
+        private TextBox txt_Type;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,403 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace ImageSlider
+{
+    public partial class Form1 : Form
+    {
+     
+        public string CheckClose = string.Empty;
+        /// </summary>
+        private int _ZoomFactor;
+        public Form1()
+        {
+            InitializeComponent();
+            txt_Folder.Text = "I:\\GFEOF";
+            txt_Count.Text = "50";
+            txt_Timer.Text = "2000";
+        }
+
+      
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            Start_Process();    
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Start_Process()
+        {
+            txt_Folder.Text = "I:\\GFEOF";
+            
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            btn_Start.BringToFront();
+            btn_Stop.BringToFront();
+            txt_Count.BringToFront();
+            txt_Counter.BringToFront();
+            txt_Height.BringToFront();
+            txt_Width.BringToFront();
+            label1.BringToFront();
+            label2.BringToFront();
+            
+
+
+            pictureBox1.SendToBack();
+            pictureBox1.BackColor = Color.Transparent;
+
+            pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            List<Image> images = new List<Image>();
+            //DirectoryInfo di = new DirectoryInfo(txt_Folder.Text); // give path
+            //FileInfo[] finfos = di.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+         
+
+            Random _rnd = new Random();
+            int cntr = 0;
+            while (cntr < Convert.ToInt32(txt_Count.Text))
+            {
+
+                if (this.CheckClose == string.Empty)
+                {
+                    string fileName = getrandomfile();
+                    var img = Image.FromFile(fileName);
+                    Logger("--------------" );
+                    Logger("New File   : " + fileName);
+
+                    btn_Start.Visible = true;
+                    btn_Start.Refresh();
+                    btn_Stop.Visible = true;
+                    btn_Stop.Refresh();
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label1.Refresh();
+                    label2.Refresh();
+                    txt_Count.Visible = true;
+                    txt_Count.Refresh();
+                    txt_Folder.Visible = true;
+                    txt_Folder.Refresh();
+                    txt_Timer.Visible = true;
+                    txt_Timer.Refresh();
+
+                    txt_FileName.Text = fileName;
+                    txt_FileName.Refresh();
+                    if (img.Width > img.Height)
+                    {
+                        txt_Width.BackColor = Color.Red;
+                        txt_Width.ForeColor = Color.White;
+                        txt_Height.BackColor = Color.Blue;
+                        txt_Height.ForeColor = Color.White;
+
+                    }
+                    else
+                    {
+                        txt_Width.BackColor = Color.Blue;
+                        txt_Width.ForeColor = Color.White;
+                        txt_Height.BackColor = Color.Red;
+                        txt_Height.ForeColor = Color.White;
+                    }
+                    txt_Width.Text = img.Width.ToString();
+                    txt_Height.Text = img.Height.ToString();
+                    txt_Width.Refresh();
+                    txt_Height.Refresh();
+
+                    if ((img.Width > 2500) || (img.Height > 2000))
+                    {
+                        Image n_img = img;
+
+                        if (img.Height < img.Width)
+                        {
+                            Image l_image = resizeImage(img, new Size(img.Width - 900, img.Height - 500));
+                            Logger("Show Pic L : " + fileName);
+                            Landscape(l_image);
+                            txt_Type.Text = "Landscape";
+                            txt_Type.Refresh();
+                        }
+                        else
+                        {
+                            try
+                            {
+                                Image p_image = resizeImage(img, new Size((img.Width / 3) + 100, (img.Height / 3) + 100));
+                                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                                pictureBox1.Image = p_image;
+                                pictureBox1.Refresh();
+                                Logger("Show Pic P : " + fileName);
+
+                                txt_Type.Text = "Portrait";
+                                txt_Type.Refresh();
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger("Error in Landscape : " + ex.Message);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                        pictureBox1.Image = img;
+                        Logger("Show Pic N : " + fileName);
+                        pictureBox1.Refresh();
+                        txt_Type.Text = "Normal";
+                        txt_Type.Refresh();
+                    }
+
+                    int milliseconds = Convert.ToInt32(txt_Timer.Text);
+                    Thread.Sleep(milliseconds);
+                    pictureBox1.Image = null;
+                    pictureBox1.ImageLocation = null;
+                    pictureBox1.Update();   
+
+                    cntr++;
+                    txt_Counter.Text = cntr.ToString();
+                    txt_Counter.Refresh();
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            
+            Logger("Loop " + cntr.ToString());
+            this.Close();
+        }
+
+        private void Landscape(Image img)
+        {
+        try { 
+            // sourceWidth and sourceHeight store the original image's width and height
+            // targetWidth and targetHeight are calculated to fit into the picImage picturebox.
+            int sourceWidth = img.Width;
+            int sourceHeight = img.Height;
+            int targetWidth;
+            int targetHeight;
+            double ratio;
+
+            // Calculate targetWidth and targetHeight, so that the image will fit into
+            // the picImage picturebox without changing the proportions of the image.
+            if (sourceWidth > sourceHeight)
+            {
+                // Set the new width
+                targetWidth = img.Width;
+                // Calculate the ratio of the new width against the original width
+                ratio = (double)targetWidth / sourceWidth;
+                // Calculate a new height that is in proportion with the original image
+                targetHeight = (int)(ratio * sourceHeight);
+            }
+            else if (sourceWidth < sourceHeight)
+            {
+                // Set the new height
+                targetHeight = img.Height;
+                // Calculate the ratio of the new height against the original height
+                ratio = (double)targetHeight / sourceHeight;
+                // Calculate a new width that is in proportion with the original image
+                targetWidth = (int)(ratio * sourceWidth);
+            }
+            else
+            {
+                // In this case, the image is square and resizing is easy
+                targetHeight = img.Height;
+                targetWidth = img.Width;
+            }
+
+            // Calculate the targetTop and targetLeft values, to center the image
+            // horizontally or vertically if needed
+            int targetTop = (img.Height - targetHeight) / 2;
+            int targetLeft = (img.Width - targetWidth) / 2;
+
+            // Create a new temporary bitmap to resize the original image
+            // The size of this bitmap is the size of the img picturebox.
+            Bitmap tempBitmap = new Bitmap(img.Width, img.Height, PixelFormat.Format24bppRgb);
+            //Bitmap tempBitmap = new Bitmap(2000, 1080, PixelFormat.Format24bppRgb);
+
+            // Set the resolution of the bitmap to match the original resolution.
+            tempBitmap.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+            // Create a Graphics object to further edit the temporary bitmap
+            Graphics bmGraphics = Graphics.FromImage(tempBitmap);
+
+            // First clear the image with the current backcolor
+
+
+            // Set the interpolationmode since we are resizing an image here
+            bmGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            // Draw the original image on the temporary bitmap, resizing it using
+            // the calculated values of targetWidth and targetHeight.
+            bmGraphics.DrawImage(img,
+                                 new Rectangle(targetLeft, targetTop, targetWidth, targetHeight),
+                                 new Rectangle(0, 0, sourceWidth, sourceHeight),
+                                 GraphicsUnit.Pixel);
+
+            // Dispose of the bmGraphics object
+            bmGraphics.Dispose();
+
+            // Set the image of the picImage picturebox to the temporary bitmap
+            pictureBox1.Image = tempBitmap;
+            pictureBox1.Refresh();
+        }
+        catch (Exception ex)
+        {
+                Logger("Error in Landscape : " + ex.Message);
+        }
+}
+
+        /// <summary>
+        /// Updates the pictureBox1 image to show the portion of the main image
+        /// the mouse is currently over.
+        /// </summary>
+   
+       
+        public void Portrait(Image img)
+        {
+            try
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+                bool source_is_wider = (float)img.Width / img.Height > (float)pictureBox1.Width / pictureBox1.Height;
+
+                var resized = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                var g = Graphics.FromImage(resized);
+                var dest_rect = new Rectangle(0, 0, 900, 1080);
+                Rectangle src_rect;
+
+                if (source_is_wider)
+                {
+                    float size_ratio = (float)img.Height / img.Height;
+                    int sample_width = (int)(pictureBox1.Width / size_ratio);
+                    src_rect = new Rectangle((img.Width - sample_width) / 2, 0, sample_width, img.Height);
+                }
+                else
+                {
+                    float size_ratio = (float)pictureBox1.Width / img.Width;
+                    int sample_height = (int)(pictureBox1.Height / size_ratio);
+                    src_rect = new Rectangle(0, (img.Height - sample_height) / 2, img.Width, sample_height);
+                }
+
+                g.DrawImage(img, dest_rect, src_rect, GraphicsUnit.Pixel);
+                g.Dispose();
+                pictureBox1.Image = resized;
+                pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+                pictureBox1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                Logger("Error in Portrait : " + ex.Message);
+            }
+        }
+
+        private string getrandomfile()
+        {
+            string file = null;
+            if (!string.IsNullOrEmpty("I:\\GFEOF"))
+            {
+                var extensions = new string[] { ".png", ".jpg", ".gif" };
+                try
+                {
+                    var di = new DirectoryInfo("I:\\GFEOF");
+                    var rgFiles = di.GetFiles("*.*").Where(f => extensions.Contains(f.Extension.ToLower()));
+                    Random R = new Random();
+                    file = rgFiles.ElementAt(R.Next(0, rgFiles.Count())).FullName;
+                }
+                // probably should only catch specific exceptions
+                // throwable by the above methods.
+                catch(Exception ex) {
+                    Logger("Error in Random File : " + ex.Message);
+                }
+            }
+            return file;
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        
+
+        private void UpdateZoomedImage(MouseEventArgs e)
+        {
+            // Calculate the width and height of the portion of the image we want
+            // to show in the pictureBox1 picturebox. This value changes when the zoom
+            // factor is changed.
+            int zoomWidth = pictureBox1.Width / _ZoomFactor;
+            int zoomHeight = pictureBox1.Height / _ZoomFactor;
+
+            // Calculate the horizontal and vertical midpoints for the crosshair
+            // cursor and correct centering of the new image
+            int halfWidth = zoomWidth / 2;
+            int halfHeight = zoomHeight / 2;
+
+            // Create a new temporary bitmap to fit inside the pictureBox1 picturebox
+            Bitmap tempBitmap = new Bitmap(zoomWidth, zoomHeight, PixelFormat.Format24bppRgb);
+
+            // Create a temporary Graphics object to work on the bitmap
+            Graphics bmGraphics = Graphics.FromImage(tempBitmap);
+
+            // Clear the bitmap with the selected backcolor
+            //bmGraphics.Clear(_BackColor);
+
+            // Set the interpolation mode
+            bmGraphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            // Draw the portion of the main image onto the bitmap
+            // The target rectangle is already known now.
+            // Here the mouse position of the cursor on the main image is used to
+            // cut out a portion of the main image.
+            bmGraphics.DrawImage(pictureBox1.Image,
+                                 new Rectangle(0, 0, zoomWidth, zoomHeight),
+                                 new Rectangle(e.X - halfWidth, e.Y - halfHeight, zoomWidth, zoomHeight),
+                                 GraphicsUnit.Pixel);
+
+            // Draw the bitmap on the pictureBox1 picturebox
+            pictureBox1.Image = tempBitmap;
+
+            // Draw a crosshair on the bitmap to simulate the cursor position
+            bmGraphics.DrawLine(Pens.Black, halfWidth + 1, halfHeight - 4, halfWidth + 1, halfHeight - 1);
+            bmGraphics.DrawLine(Pens.Black, halfWidth + 1, halfHeight + 6, halfWidth + 1, halfHeight + 3);
+            bmGraphics.DrawLine(Pens.Black, halfWidth - 4, halfHeight + 1, halfWidth - 1, halfHeight + 1);
+            bmGraphics.DrawLine(Pens.Black, halfWidth + 6, halfHeight + 1, halfWidth + 3, halfHeight + 1);
+
+            // Dispose of the Graphics object
+            bmGraphics.Dispose();
+
+            // Refresh the pictureBox1 picturebox to reflect the changes
+            pictureBox1.Refresh();
+        }
+
+        public static void VerifyDir(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+            }
+            catch { }
+        }
+
+        public static void Logger(string lines)
+        {
+            string path = "D:/Log/";
+            VerifyDir(path);
+            string fileName = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "_Logs.txt";
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(path + fileName, true);
+                file.WriteLine(DateTime.Now.ToString() + ": " + lines);
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+
+    }
+
+
+}
